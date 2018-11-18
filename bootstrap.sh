a92b39e1b590657f3b3f8c24fc353f39e4ad833d
Cláudio Pereira <git@claudiop.com>
2018-11-18 22:22:34 +0000

    Better bootstrap. Add nginx.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,62 @@
 #!/bin/bash
 
-if [ -z $1 -o $1 == "--help" ]; then
+BUILD_IMAGE=false
+PULL_IMAGES=false
+CREATE_LAUNCHER=true
+
+usage() {
   echo "Usage:"
   echo "./bootstrap.sh /installation/path"
-  exit
+  echo "If this is your first time running this, might be wise to read this script's code."
+  echo "There are a few options I could not be bothered to implement as flags."
+  exit 1
+}
+
+bootstrap() {
+    echo "Installing onto $1"
+    echo "Creating folder structure"
+    mkdir -p $1/source $1/log $1/configurations $1/databases
+
+    echo "Cloning Kleep git"
+    git clone --quiet https://gitlab.com/claudiop/Kleep.git $1/source/
+
+    echo "Copying configurations"
+    cp -r configurations $1
+    mv $1/configurations/settings.json $1/source/
+
+    echo "Creating empty vulnerability database"
+    sqlite3 vulnerabilities.db < kleep/vulnerabilities.sql
+    mv vulnerabilities.db $1/databases/
+
+    if [ "$BUILD_IMAGE" = true ] ; then
+        echo "Building docker image"
+        docker build  -t claudioap/kleep .
+    else
+        echo "Skipped docker image build"
+    fi
+
+    if [ "$PULL_IMAGES" = true ] ; then
+        echo "Downloading other required images"
+        docker pull nginx
+    else
+        echo "Skipped pulling other images"
+    fi
+
+    if [ "$CREATE_LAUNCHER" = true ] ; then
+        echo "Creating launch script (launch.sh in the destination folder)"
+        cd $1
+        touch launch.sh
+        echo "docker run --rm -d -p 1893:1893 -v $(pwd):/kleep claudioap/kleep" > launch.sh
+        echo "docker run --rm -d -p 80:80 -v $(pwd)/html:/srv/http:ro -v $(pwd)/configurations/nginx:/etc/nginx:ro nginx" >> launch.sh
+        chmod +x launch.sh
+    fi
+}
+
+if [ -z $1 ] || [ "$1" == "--help" ]; then
+    usage
+elif [ ! -v $2 ]; then
+    echo "Too many arguments"
+    usage
+else
+    bootstrap $1
 fi
-
-if [ ! -v $2 ]; then
-  echo "Too many arguments"
-  exit
-fi
-
-mkdir -p $1
-
-mkdir $1/source
-mkdir $1/log
-git clone https://gitlab.com/claudiop/Kleep.git $1/source/
-cp configurations/settings.json $1/source/
-cp vulnerabilities.db $1/
-cp configurations/uwsgi/kleep.ini $1/uwsgi.ini
-sqlite3 vulnerabilities.db < vulnerabilities.sql
